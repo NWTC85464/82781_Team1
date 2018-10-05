435e7ddd13ffd946c1e1624225e1bb3190e159ff
Sean Monthei <smonty2290@gmail.com>
2018-10-05 14:21:44 -0500

    Version1

---
+++ RestaurantPOS/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantPOS
+{
+    class Employee
+    {
+        private String name;
+        private int employeeNumber;
+        private int employeePassCode;
+        private String jobTitle;
+
+        public Employee(String _name, int _employeeNumber, int _employeePassCode, String _jobTitle)
+        {
+            this.name = _name;
+            this.employeeNumber = _employeeNumber;
+            this.employeePassCode = _employeePassCode;
+            this.jobTitle = _jobTitle;
+        }
+    }
+}
