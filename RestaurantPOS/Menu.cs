d0c4d4f7ef2c3c4825377a71b94070a4a88ca6d6
Sean Monthei <smonty2290@gmail.com>
2018-11-02 11:30:31 -0500

    Order class refactored and edited to OrderItems to reflect data model Menu Class added

---
+++ RestaurantPOS/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Menu
+{
+    private String menuName;
+    public List<MenuItems> menuList;
+
+
+    public Menu(String _menuName)
+	{
+        MenuName = _menuName;
+	}
+
+    public string MenuName { get => menuName; set => menuName = value; }
+
+}
